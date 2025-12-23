3d2ccbfa9a91fab9b90d1bd7229cef46c24c455b
shakya-ctrl77 <shakyasanjay944@gmail.com>
2025-12-23 11:00:27 +0545

    day 2 of internship

---
+++ day2.py
@@ -0,0 +1,173 @@
+# function  in python 
+
+'''def greet():
+
+    print("hello")
+
+greet()'''
+
+# function with parameters 
+
+'''def add(a,b):
+    print(a+b)
+add(8,9)
+'''
+# numbers
+'''
+x=88.56
+
+a=int(x)
+
+print(x)
+'''
+
+# random number
+
+'''import random
+
+print(random.randrange(1,9))
+
+'''
+
+# casting
+
+'''x=float(2)
+
+print(x)'''
+
+# string in python 
+'''a= "hello , world"
+print(a[4])
+'''
+'''txt="bus is delayed"
+if 'bus' in txt:
+ print('bus')'''
+
+# string slicing 
+'''b = "Hello, World!"
+print(b[-6:-2])'''
+
+# modifying string
+
+# a=" ranjay "
+# print(a.replace("r","S") and a.strip())
+
+'''a= 'python.hacker'
+print(a.split("."))
+'''
+# string concatenation 
+
+'''a="sanjay"
+b="shakya"
+d="hacker"
+c=a+ " "+ b +" "+d
+
+print(c)'''
+
+# string formatting 
+
+'''age = 36
+txt = "My name is John, I am " + age
+# throws error
+print(txt)
+'''
+'''amt = 36000
+txt = f"My name is John, I have {amt} salary"
+print(txt)
+'''
+
+'''price=44.66
+text=f"the price of the grocery will be{price:.3f}"
+print(text)
+'''
+'''txt=f"calculation {22*3}"
+print(txt)'''
+
+# escape character
+
+'''txt = "python are the so-called \"Extra ordinary \" from the north."
+print(txt)'''
+
+'''Not="our new teacher said\"let\'s have a game tommorow\".We indeed accepted it"
+print(Not)
+'''
+'''txt="sanjay"
+print(txt.center(5))'''
+
+
+
+'''bool(["apple", "cherry", "banana"])'''
+
+# python operator
+
+'''x=13
+y=5
+
+print(x//y)'''
+
+#  chain comparison operator 
+
+'''x=0
+print(1 < x <10)'''
+
+# python list
+
+'''x=frozenset({'mouse ','keyboard','speaker'}) 
+
+print(frozenset)'''
+
+
+# frozenset 
+
+'''ingredients = frozenset(["flour", "sugar", "eggs", "milk"])
+print("Flour in recipe?", "flour" in ingredients)
+'''
+
+
+# LIST IN PYTHON 
+'''mylist=['ball','bat','wicket','ball','tire']
+
+print(mylist)
+
+print(len(mylist))
+
+mylist=['apple',55,'banana','cucumber']
+print(type(mylist))'''
+
+'''mylist = ['apple', 'banana', 'cherry','blackberry','avocado','melon','lemon']
+print(mylist[0:])'''
+
+'''thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])'''
+
+# change the list item 
+
+list=["rabin","sabin","bibek"]
+list[2]="ranjan"
+
+print(list)
+
+
+list=["aprilia","vespa","yamaha","honda","hero","zoom"]
+list[1:4]="tvs","volkswagen","rolls"
+print(list)
+
+list=["messi","neymar","cavani"]
+list[1:3]="CR7",
+print(list)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[2]=("watermelon")
+print(thislist[1])
+
+
+# append the list in python 
+list=['argentina','spain','canada','philipine']
+list.append('portugal')
+print(list)
+
+
+
+
+
+
