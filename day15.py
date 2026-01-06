bed0aa9522137797e3b0d6fba110034538c644c0
shakya-ctrl77 <shakyasanjay944@gmail.com>
2026-01-06 10:39:12 +0545

    day15 of internship

---
+++ day15.py
@@ -0,0 +1,16 @@
+'''class parent:
+    def parentmethod(self):
+        print('calling parent method')
+        
+class child(parent):
+    def childmethod(self):
+        print('calling child method ')
+        
+c=child()
+c.childmethod()
+c.parentmethod()
+'''
+
+# multiple inheritance 
+
+
